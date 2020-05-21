83a6215a84125412c7b603574384df44a05f7873
Zeljko-js <novakzex@hotmail.com>
2020-05-21 18:01:43 +0200

    first task end

--- src/components/Field.vue
+++ src/components/Field.vue
@@ -4,15 +4,18 @@
         {{ name }}
       </div>
       <div class="field_content">
-        <div class="field_errow">
-          <p>^</p>
+        <div class="field_errow" v-if="increase">
+          <img src="../assets/arrow-up.jpg" class="errow_img"/>
+        </div>
+        <div class="field_errow" v-else>
+          <img src="../assets/arrow-down.jpg" class="errow_img"/>
         </div>
         <div class="field_value">
-          <p>3</p>
+          {{ value }}
         </div>
       </div>
-      <button @:click="stop()">Stop</button>
-      <button @:click="start()">Start</button> 
+      <button v-if="valueStart" v-on:click="stop()">Stop</button>
+      <button v-else v-on:click="start()">Start</button>
   </div>
 </template>
 
@@ -22,5 +25,48 @@ export default {
   props: {
     name: String
   },
-}
+  data () {
+    return {
+      value: 3,
+      valueStart: true,
+      interval: null,
+      increase: true
+    };
+  },
+  methods: {
+    start() {
+      this.interval = setInterval(() => {
+        this.valueStart = true;
+        let v = this.generateValue();
+        const sign = this.generateValue();
+        if (sign > 1.4) {
+          this.increase = true;
+        } else {
+          v = v * -1;
+          this.increase = false;
+        }
+
+        // TO-DO:  push to history array
+
+        this.value = Math.round((this.value + v) * 10) / 10;
+      }, 2000);
+    },
+    stop() {
+      this.valueStart = false;
+      clearInterval(this.interval);
+    },
+    generateValue() {
+      return Math.round((Math.random() + 1) * 10) / 10;
+    }
+  },
+  mounted() {
+
+    // TO-DO: get last value
+
+    this.start();
+  },
+  beforeDestroy() {
+      this.stop();
+  }
+};
 </script>
